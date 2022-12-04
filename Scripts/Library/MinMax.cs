126d9147f6065cf08500736954eedea926ecb8c6
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2022-12-04 10:20:12 +0100

    Solving Git

---
+++ Scripts/Library/MinMax.cs
@@ -0,0 +1,16 @@
+public struct MinMax
+{
+    public MinMax(int _number1, int _number2)
+    {
+        bool _firstGreater = _number1 >= _number2;
+        min = _firstGreater ? _number2 : _number1;
+        max = _firstGreater ? _number1 : _number2;
+    }
+
+    public readonly int min, max;
+
+    public bool Contains(int _value) => _value >= min && _value <= max;
+    public bool Contains(MinMax _content) => Contains(_content.min) && Contains(_content.max);
+    public bool Overlap(MinMax _content) => Contains(_content.min) || Contains(_content.max);
+    public string Text => $"Min : {min} / Max : {max}";
+}
